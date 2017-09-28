0714f806314752975c4503dc3f96d0e3f387b1fb
Tomasz Trebski <kornicameister@gmail.com>
2017-09-28 19:08:10 +0200

    Move vim to dev/

--- dev/all.sh
+++ dev/all.sh
@@ -6,6 +6,7 @@ K_DIR=${2}
 source ${TOP_DIR}/utils.sh
 
 # dev tools
+source ${TOP_DIR}/dev/vim.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/node.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/git.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/gerrit.sh "${TOP_DIR}" "${K_DIR}"
@@ -14,6 +15,7 @@ source ${TOP_DIR}/dev/bash.sh "${TOP_DIR}" "${K_DIR}"
 # dev tools
 
 install_dev() {
+    install_prompt vim install_dev_vim
     install_prompt bash install_dev_bash
     install_prompt git install_dev_git
     install_prompt node install_dev_node
