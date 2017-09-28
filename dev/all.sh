07041ff137c741a5e8ccd01b0683a6ed341a7b0b
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-09-28 07:25:13 +0200

    Enable gerrit-cli

--- dev/all.sh
+++ dev/all.sh
@@ -8,10 +8,12 @@ source ${TOP_DIR}/utils.sh
 # dev tools
 source ${TOP_DIR}/dev/node.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/git.sh "${TOP_DIR}" "${K_DIR}"
+source ${TOP_DIR}/dev/gerrit.sh "${TOP_DIR}" "${K_DIR}"
 # dev tools
 
 install_dev() {
     install_prompt git install_dev_git
     install_prompt node install_dev_node
+    install_prompt gerrit install_dev_gerrit
 }
 
