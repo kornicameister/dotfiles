4117c6ab846f4612e1ca099744c7c23ad25246bd
Tomasz Trębski <kornicameister@gmail.com>
2018-05-07 01:04:35 +0200

    Drop useless stuff

--- dev/all.sh
+++ dev/all.sh
@@ -12,9 +12,6 @@ source ${TOP_DIR}/dev/node.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/git.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/gerrit.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/python.sh "${TOP_DIR}" "${K_DIR}"
-source ${TOP_DIR}/dev/bash.sh "${TOP_DIR}" "${K_DIR}"
-source ${TOP_DIR}/dev/ruby.sh "${TOP_DIR}" "${K_DIR}"
-source ${TOP_DIR}/dev/travis.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/elm.sh "${TOP_DIR}" "${K_DIR}"
 # dev tools
 
@@ -23,11 +20,8 @@ install_dev() {
     install_prompt python install_dev_python
     install_prompt vim install_dev_vim
     install_prompt fzf install_dev_fzf
-    install_prompt bash install_dev_bash
     install_prompt node install_dev_node
     install_prompt gerrit install_dev_gerrit
-    install_prompt ruby install_dev_ruby
-    install_prompt travis install_dev_travis
     install_prompt elm install_dev_elm
     install_prompt mdv _install_mdv
 }
