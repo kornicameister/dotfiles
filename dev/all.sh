68fc5ec57b6049ca39e08025f19e321cc502e42b
Tomasz Trębski <kornicameister@gmail.com>
2018-05-16 00:02:28 +0200

    Add pyenv and necessary plugins

--- dev/all.sh
+++ dev/all.sh
@@ -10,12 +10,10 @@ source ${TOP_DIR}/dev/vim.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/node.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/git.sh "${TOP_DIR}" "${K_DIR}"
 source ${TOP_DIR}/dev/gerrit.sh "${TOP_DIR}" "${K_DIR}"
-source ${TOP_DIR}/dev/python.sh "${TOP_DIR}" "${K_DIR}"
 # dev tools
 
 install_dev() {
     install_prompt git install_dev_git
-    install_prompt python install_dev_python
     install_prompt vim install_dev_vim
     install_prompt node install_dev_node
     install_prompt gerrit install_dev_gerrit
