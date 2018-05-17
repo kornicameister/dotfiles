04756f7583fd7626c8ea55b49491d35c341a83ad
Tomasz Trębski <kornicameister@gmail.com>
2018-05-17 23:05:44 +0200

    Add concept of post-install hooks

--- pyenv.sh
+++ pyenv.sh
@@ -2,10 +2,6 @@
 export PYENV_ROOT=$(realpath "$HOME/.pyenv")
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-[ ! -d $(pyenv root)/plugins/pyenv-update ] && git clone git://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
-[ ! -d $(pyenv root)/plugins/pyenv-virtualenv ] && git clone git://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-[ ! -d $(pyenv root)/plugins/pyenv-ccache ] && git clone git://github.com/pyenv/pyenv-ccache.git $(pyenv root)/plugins/pyenv-ccache
-
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
