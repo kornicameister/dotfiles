630a6ff02dd1881dc72c6830e0ed2d23fc74b820
Tomasz Trębski <kornicameister@gmail.com>
2018-05-21 00:04:47 +0200

    Move to nodenv to speed up

--- nvm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export NVM_DIR=$(realpath "$HOME/.nvm")
-export NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist
-
-[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"  # This loads nvm
-[ -s "${NVM_DIR}/bash_completion" ] && source "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
-
