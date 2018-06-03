d9121ddf341f52df8c8724dab4f59c9997444293
Tomasz Trębski <kornicameister@gmail.com>
2018-06-03 14:24:32 +0200

    Move git commit browser into the bin/

--- bash/fn.sh
+++ bash/fn.sh
@@ -9,18 +9,6 @@ docker_gc() {
         -v /etc:/etc:ro spotify/docker-gc
 }
 
-# fshow - git commit browser
-fshow() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --height 95% --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-      --header "Press CTRL-S to toggle sort" \
-      --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
-                 xargs -I % sh -c 'git show --color=always % | head -200 '" \
-      --bind "enter:execute:echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
-              xargs -I % sh -c 'vim fugitive://\$(git rev-parse --show-toplevel)/.git//% < /dev/tty'"
-}
-
 # git related
 
 # Public: Removes all .orig files after rebase/merge/cherry-pick ops
