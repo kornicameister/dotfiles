e07306e2eb3bd4606f4bf55befce9f12fe6b6b55
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2018-06-13 07:30:29 +0200

    Add t - simple todo manager

---
+++ t.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# contains configuration for https://github.com/sjl/t
+export T_DIR=$HOME/.cache/tasks
+
+[ ! -d $T_DIR ] && mkdir -p $T_DIR
+alias t='t --task-dir $T_DIR --list tasks'
