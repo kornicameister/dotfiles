8993b9312d3e11b7b1e858b9bb66076e417adcd1
Tomasz Trębski <kornicameister@gmail.com>
2018-11-22 20:58:35 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+PWD=${PWD}
+
+echo "kornicameister/dotfiles installer"
+
+sudo true
+sudo apt-get update -q
+sudo apt-get install git -y -q
+
+echo "I think I have everything I need, dotfiles will be installed in the directory from which script was called"
+
+git clone https://github.com/kornicameister/dotfiles.git
+pushd ./dotfiles
+
+git submodule update --init --recursive
+./install
+
+popd
