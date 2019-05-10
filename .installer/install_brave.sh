cefa9ad5254876ca726dd7bb04b435fee590210a
Tomasz Trębski <kornicameister@gmail.com>
2019-05-10 23:12:46 +0200

    Fix brave installation

--- .installer/install_brave.sh
+++ .installer/install_brave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s https://brave-browser-aptitude-beta.s3.brave.com/brave-core-nightly.asc |  aptitude-key --keyring /etc/aptitude/trusted.gpg.d/brave-browser-beta.gpg add -
-echo "deb [arch=amd64] https://brave-browser-aptitude-beta.s3.brave.com/ trusty main" |  tee /etc/aptitude/sources.list.d/brave-browser-beta-trusty.list
+curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc |  apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-beta.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ trusty main" |  tee /etc/apt/sources.list.d/brave-browser-beta-trusty.list
 
 aptitude update -q -y
 aptitude install -y -V -D -Z brave-browser-beta
