411f18152e98cc236592507747e91f7eff23c523
Tomasz Trębski <kornicameister@gmail.com>
2019-05-12 00:28:21 +0200

    fixup! Make brava installation distro aware

--- .installer/install_brave.sh
+++ .installer/install_brave.sh
@@ -8,7 +8,7 @@ if [[ "${distro}" == "LinuxMint" ]]; then
   echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ trusty main" |  tee /etc/apt/sources.list.d/brave-browser-beta-trusty.list
 elif [[ "${distro}" == "Ubuntu" ]]; then
   source /etc/os-release
-  echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/brave-browser-beta-${UBUNTU_CODENAME}.list
+  echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ $UBUNTU_CODENAME main" | tee "/etc/apt/sources.list.d/brave-browser-beta-${UBUNTU_CODENAME}.list"
 fi
 
 aptitude update -q -y
