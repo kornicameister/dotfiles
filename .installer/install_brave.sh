6bf170a843b13f441ddfac3f6af9364b00b9b0f0
Tomasz Trębski <kornicameister@gmail.com>
2019-05-14 22:08:21 +0200

    F

--- .installer/install_brave.sh
+++ .installer/install_brave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 distro=$(lsb_release -si 2>&1)
 
 curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc |  apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-beta.gpg add -
@@ -13,3 +13,4 @@ fi
 
 aptitude update -q -y
 aptitude install -y -V -D -Z brave-browser-beta
+set +x
