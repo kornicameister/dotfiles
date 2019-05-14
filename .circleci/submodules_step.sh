8063380e4c34049818e7e609aeedb15ed52d1708
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2019-05-14 08:13:28 +0200

    fixup! Better cmp for submodules_step

--- .circleci/submodules_step.sh
+++ .circleci/submodules_step.sh
@@ -3,10 +3,9 @@
 mods=$(git diff --stat --name-only HEAD..master)
 mods_count=$(echo "${mods}" | grep -cE '\.gitmodules|dependencies\/')
 
-(( mods_count >= 1)) && {
-  echo "Mods are :: ${mods}"
+echo "Mods are :: ${mods}"
 
+(( mods_count >= 1)) && {
   git submodule update --init --recursive --recommend-shallow;
   git submodule status
-
 } || echo "Nothing about submodules have changed, skipping"
