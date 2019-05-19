cc34562d931273bbe3f4646062a798bd0a0a1648
Tomasz Trębski <kornicameister@gmail.com>
2019-05-20 00:01:50 +0200

    Add more commands to check

--- .circleci/verify_install.sh
+++ .circleci/verify_install.sh
@@ -3,19 +3,19 @@
 set -e
 
 info () {
-  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "${1}"
+  printf "\\r  [ \033[00;34m..\033[0m ] %s\n" "${1}"
 }
 
 user () {
-  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "${1}"
+  printf "\\r  [ \033[0;33m??\033[0m ] %s\n" "${1}"
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "${1}"
+  printf "\\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "${1}"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "${1}"
+  printf "\\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "${1}"
   echo ''
   exit 666
 }
@@ -28,6 +28,15 @@ validate_bin_accessible() {
     snap
     git
     git-lfs
+    zsh
+    fzf
+    pyenv
+    nodenv
+    python2
+    python3
+    purge-old-kernels
+    terminator
+    git-extras
   );
 
   for bin in "${bins_to_check[@]}"; do
