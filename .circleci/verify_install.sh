a1db978cb8c5ff9f55bdf2ea1573c3f422d55cc8
Tomasz Trębski <kornicameister@gmail.com>
2019-05-19 23:57:34 +0200

    Add tests for install

---
+++ .circleci/verify_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "${1}"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "${1}"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "${1}"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "${1}"
+  echo ''
+  exit 666
+}
+
+validate_bin_accessible() {
+  bins_to_check=(
+    nvim
+    docker
+    docker-compose
+    snap
+    git
+    git-lfs
+  );
+
+  for bin in "${bins_to_check[@]}"; do
+    if command -v "${bin}" >/dev/null 2>&1; then
+      success "${bin} is accessible via $(whereis "${bin}")"
+    else
+      fail "${bin} is not accessible"
+    fi
+  done
+}
+
+info 'Validating installation'
+validate_bin_accessible
