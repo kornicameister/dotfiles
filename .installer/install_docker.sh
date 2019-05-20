0f1c1c69688076f09cc171f1d102380293e8e41e
Tomasz Trębski <kornicameister@gmail.com>
2019-05-20 22:03:24 +0200

    Update install_docker.sh

--- .installer/install_docker.sh
+++ .installer/install_docker.sh
@@ -17,7 +17,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable edge"
+   stable"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   edge"
 
 aptitude update -q -y
 aptitude install docker-ce docker-ce-cli containerd.io -y -V -D -Z
