c5f7ee98e73ff559a6699e9805cb7cd011a82abd
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2019-05-20 13:44:38 +0200

    Revert old install way

--- .installer/install_docker.sh
+++ .installer/install_docker.sh
@@ -12,7 +12,15 @@ aptitude remove \
     containerd \
     -y -V -D -Z
 
-curl -fsSL get.docker.com | sh
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+aptitude update -q -y
+aptitude install docker-ce docker-ce-cli containerd.io -y -V -D -Z
 
 # docker-compose
 compose_version=$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\\.[0-9][0-9]+\\.[0-9]+$" | tail -n 1)
