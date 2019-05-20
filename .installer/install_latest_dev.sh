5339261f707e16414b17b12f90ef9f54b1359c39
Tomasz Trębski <kornicameister@gmail.com>
2019-05-20 20:40:15 +0200

    fixup! Push installing node & pyenv to .installer

--- .installer/install_latest_dev.sh
+++ .installer/install_latest_dev.sh
@@ -8,12 +8,17 @@ if [ ! -f "$(pyenv root)/version" ]; then
     pyenv latest install -s 2
     pyenv latest install -s 3
     pyenv latest --print 3 >> "$(pyenv root)/version"
+else
+    echo "pyenv already has system version set at it is $(cat $(pyenv root)/version)"
 fi
+
 if [ ! -f "$(nodenv root)/version" ]; then
     echo "Installing latest node"
 
     nodenv latest install -s 12
     nodenv latest --print 12 >> "$(nodenv root)/version"
+else
+    echo "nodenv already has system version set at it is $(cat $(nodenv root)/version)"
 fi
 
 set +x
