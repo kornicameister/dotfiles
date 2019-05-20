d7546cc08ffaaf3bc7a8faec6f3574e8afdd720c
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2019-05-20 13:54:56 +0200

    Push installing node & pyenv to .installer

---
+++ .installer/install_latest_dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+if [ ! -f "$(pyenv root)/version" ]; then
+    echo "Installing latest python"
+
+    pyenv latest install -s 2
+    pyenv latest install -s 3
+    pyenv latest --print 3 >> "$(pyenv root)/version"
+fi
+if [ ! -f "$(nodenv root)/version" ]; then
+    echo "Installing latest node"
+
+    nodenv latest install -s 12
+    nodenv latest --print 12 >> "$(nodenv root)/version"
+fi
