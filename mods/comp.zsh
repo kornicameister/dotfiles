e623fbd8cbc80ee56c2c5a7e9556d901458e2f8a
Tomasz Trębski <kornicameister@gmail.com>
2019-09-24 00:01:05 +0200

    fixup! Migrate to zplug

--- mods/comp.zsh
+++ mods/comp.zsh
@@ -1,5 +1,16 @@
 autoload -Uz compinit
 
+setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
+setopt PATH_DIRS           # Perform path search even on command names with slashes.
+setopt AUTO_MENU           # Show completion menu on a successive tab press.
+setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
+setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
+setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit
+unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
+unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
+unsetopt CASE_GLOB         # Case insensitive
+
 # cache for every 8 hours
 _comp_files=(${ZDOTDIR:-$HOME}/.zcompdump(Nm-8))
 if (( $#_comp_files )); then
@@ -15,7 +26,6 @@ zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
 
 # case-insensitive, partial-word, and then substring completion.
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-unsetopt CASE_GLOB
 
 # Group matches and describe.
 zstyle ':completion:*:*:*:*:*' menu select
