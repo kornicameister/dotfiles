a49bee6bd2f39a2e66f70ccbdfac190d87fd03fd
Tomasz Trębski <kornicameister@gmail.com>
2019-11-26 09:05:00 +0100

    Update install_whatpulse.sh

--- .installer/install_whatpulse.sh
+++ .installer/install_whatpulse.sh
@@ -3,4 +3,11 @@
 arch="$( if ((1<<32)); then echo 64; else echo 32; fi;)"
 version="313"  # for ubuntu derivates
 
+aptitude install -y -qq \
+  libqt5widgets5 \
+  libqt5sql5-sqlite \
+  libqt5script5
+
 download_url="https://whatpulse.org/downloads/${version}/{$arch}bit/"
+
+# setcap cap_net_raw,cap_net_admin=eip ./whatpulse
