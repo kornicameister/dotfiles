a53efbec36e08ed261f83d9889b01f32d671b6c3
Tomasz Trębski <kornicameister@gmail.com>
2020-04-08 21:44:44 +0200

    fix formatting

--- .installer/install_brave.sh
+++ .installer/install_brave.sh
@@ -5,10 +5,10 @@ distro=$(lsb_release -si 2>&1)
 curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-beta.gpg add -
 
 if [[ "${distro}" == "LinuxMint" ]]; then
-	echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ trusty main" | tee /etc/apt/sources.list.d/brave-browser-beta-trusty.list
+  echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ trusty main" | tee /etc/apt/sources.list.d/brave-browser-beta-trusty.list
 elif [[ "${distro}" == "Ubuntu" ]]; then
-	source /etc/os-release
-	echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ $UBUNTU_CODENAME main" | tee "/etc/apt/sources.list.d/brave-browser-beta-${UBUNTU_CODENAME}.list"
+  source /etc/os-release
+  echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ $UBUNTU_CODENAME main" | tee "/etc/apt/sources.list.d/brave-browser-beta-${UBUNTU_CODENAME}.list"
 fi
 
 aptitude update -q -y
--- .installer/install_ctags.sh
+++ .installer/install_ctags.sh
@@ -3,12 +3,12 @@
 CTAGS_DIR="${PWD}/dependencies/ctags"
 
 [ ! -d "${CTAGS_DIR}" ] && (
-	cd "${K_DOTFILES_DIR}" || exit 2
-	git submodule update --init --recursive
-	cd ..
+  cd "${K_DOTFILES_DIR}" || exit 2
+  git submodule update --init --recursive
+  cd ..
 )
 [ ! -f "${HOME}/.gitconfig.local" ] && (
-	touch "${HOME}/.gitconfig.local"
+  touch "${HOME}/.gitconfig.local"
 )
 
 aptitude purge exuberant-ctags -q -y
@@ -19,13 +19,13 @@ CURRENT_VERSION="$(git config --file "${HOME}/.gitconfig.local" --default '-1' -
 REPO_VERSION="$(git log -n 1 --format=oneline | awk '{print $1}')"
 
 if [[ "${CURRENT_VERSION}" == "${REPO_VERSION}" ]]; then
-	echo "No need to install ctags again"
+  echo "No need to install ctags again"
 else
-	./autogen.sh
-	./configure --prefix="${HOME}/.local"
-	make clean
-	make
-	make install
-	git config --file="${HOME}/.gitconfig.local" ctags.version "${REPO_VERSION}"
+  ./autogen.sh
+  ./configure --prefix="${HOME}/.local"
+  make clean
+  make
+  make install
+  git config --file="${HOME}/.gitconfig.local" ctags.version "${REPO_VERSION}"
 fi
 cd ..
