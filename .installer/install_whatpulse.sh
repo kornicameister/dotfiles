9cdadaa70ed0133aa200fa3ca193c79f9df25aa9
Tomasz Trebski <tomasz.trebski@ts.fujitsu.com>
2020-05-11 12:41:24 +0200

    fixup! Add installing whatpulse

--- .installer/install_whatpulse.sh
+++ .installer/install_whatpulse.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-arch="$( if ((1<<32)); then echo 64; else echo 32; fi;)"
-version="313"  # for ubuntu derivates
+if [[ -z "${WHATPULSE_USER}" ]]; then
+  echo "WHATPULSE_USER is required"
+  exit 1
+fi
 
-aptitude install -y -qq \
+echo "user ===> ${WHATPULSE_USER}"
+
+version="313"
+
+if _=$((1 << 32)); then
+  arch=64
+else
+  arch=32
+fi
+
+aptitude install -y -q \
   libqt5widgets5 \
   libqt5sql5-sqlite \
   libqt5script5 \
   libqt5xml5 \
   libssl-dev
 
-download_url="https://whatpulse.org/downloads/${version}/{$arch}bit/"
+download_url="https://whatpulse.org/downloads/${version}/${arch}bit/" && echo "Downloading from ${download_url}"
+download_dir="$(mktemp -t -d kdotfiles-whatpulse.XXXXXXXXXX)"
+aria2c "${download_url}" -d "${download_dir}" -o whatpulse.tar.gz
 
-# setcap cap_net_raw,cap_net_admin=eip ./whatpulse
+cd "${download_dir}" && (
+  tar -xvf whatpulse.tar.gz -C "${PWD}"
+  setcap cap_net_raw,cap_net_admin=eip "${PWD}/whatpulse"
+  yes "${WHATPULSE_USER}" | sudo ./setup-input-permissions.sh
+  cp -f "${PWD}/whatpulse" "/usr/local/bin/whatpulse"
+)
+rm -rf "${download_dir}" && whatpulse --version
