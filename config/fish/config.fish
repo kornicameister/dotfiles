e92bc1072dfca36bb15f6a538e665c70e3ed9c32
kornicameister <kornicameister@gmail.com>
2023-05-05 10:22:05 +0200

    Reset fish configuration for next attempt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,23 +1,3 @@
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
-if status is-login
-  . (pyenv init --path | psub)
-end
 if status is-interactive
-  . (pyenv init - | psub)
-  . (pyenv virtualenv-init - | psub)
+    # Commands to run in interactive sessions can go here
 end
-
-set fzf_preview_dir_cmd exa --all --color=always
-
-# aliases
-alias l='exa --sort=changed --icons -la --git --git-ignore --ignore-glob=".git|__pycache__"'
-alias ls='exa --icons'
-alias la='exa --group-directories-first --icons -la'
-alias ll='exa --group-directories-first --icons -la --color-scale --time-style=long-iso --git --git-ignore --ignore-glob=".git|__pycache__"'
-alias tree='exa --group-directories-first -T --icons --level=2'
-
-set -gx PIPX_DEFAULT_PYTHON (pyenv which python)
--- config/fish/functions/gbda.fish
+++
@@ -1,5 +0,0 @@
-function gbda -d "Delete all branches merged in current HEAD"
-  git branch --merged | \
-    command grep -vE  '^\*|^\s*(master|main|develop)\s*$' | \
-    command xargs -n 1 git branch -d
-end
