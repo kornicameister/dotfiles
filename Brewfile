67b31c0205975ec173829c6fe90e0b8b9536b582
Tomasz Trębski <kornicameister@gmail.com>
2024-09-05 09:20:30 +0200

    Add MacOS setup (#1777)

---
+++ Brewfile
@@ -0,0 +1,143 @@
+# https://github.com/Homebrew/homebrew-bundle
+
+tap "homebrew/bundle"
+tap "homebrew/cask-drivers"
+tap "homebrew/cask-versions"
+tap "homebrew/services"
+tap "puma/puma"
+tap "teamookla/speedtest"
+tap "universal-ctags/universal-ctags"
+tap "ankitpokhrel/jira-cli"
+tap "jesseduffield/lazygit"
+
+# Homebrew
+# https://github.com/Homebrew/brew/
+
+# Need to install openssl before libyaml
+brew "openssl"
+
+# Base packages
+brew "coreutils"
+brew "moreutils"
+brew "findutils"
+brew "wget"
+brew "curl"
+brew "cmake"
+brew "gnu-sed"
+
+# shell
+brew "zsh"
+brew "fish", args: ['HEAD']
+
+# Development stuff
+brew "watch"
+brew "ccache"
+brew "git"
+brew "git-extras"
+brew "git-lfs"
+brew "git-filter-repo"
+brew "git-delta"
+brew "lazygit"
+brew "jq"         # reading json
+brew "jo"         # creating json
+brew "pipx"
+brew "pipenv"
+brew "shellcheck"
+brew "shfmt"
+brew "svn"
+brew "subversion"
+brew "ripgrep"
+brew "the_silver_searcher"
+brew "ripgrep"
+brew "universal-ctags"
+brew "yq"
+brew "lua"
+brew "stylua"
+brew "exiftool"
+brew "rust"
+
+cask "gitify"
+cask "meld"
+cask "iterm2"
+cask "warp"
+brew "neovim", args: ['HEAD']
+cask "visual-studio-code"
+
+brew "jenv"
+cask "temurin"
+cask "temurin8"
+brew "maven"
+
+brew "ruby"
+brew "node"
+
+# Virtual environments
+brew "rbenv"
+
+# Virtualization
+cask "docker"
+brew "ctop"       # awesome htop for containers
+brew "hadolint"
+brew "kind"
+brew "helm"
+cask "virtualbox"
+
+# Browsers
+cask "brave-browser"
+cask "google-chrome"
+cask "firefox"
+
+# Utilities
+brew "aria2"
+brew "asdf"
+brew "bat"
+brew "direnv"
+brew "eza"
+brew "fd"
+brew "fzf"
+brew "glow"
+brew "gnupg"
+brew "htop"
+brew "jira-cli"
+brew "nmap"
+brew "openssh"
+brew "p7zip"
+brew "parallel"
+brew "prettyping"
+brew "screenfetch"
+brew "timg"
+brew "viddy"
+brew "zoxide"
+cask "google-drive"
+
+# fonts
+tap "homebrew/cask-fonts"
+
+cask "font-iosevka"
+cask "font-fontawesome"
+cask "font-material-icons"
+cask "font-hack-nerd-font"
+cask "font-awesome-terminal-fonts"
+
+# cloud
+tap "aws/tap"
+tap "common-fate/granted"
+tap "fugue/regula"
+tap "hashicorp/tap"
+tap "salesforce/cloudsplaining", "https://github.com/salesforce/cloudsplaining"
+
+brew "aws-sam-cli"
+brew "awscli"
+brew "aws-nuke"
+brew "cloudsplaining"
+brew "granted"
+brew "hashicorp/tap/terraform"
+brew "hashicorp/tap/terraform-ls"
+brew "regula"
+
+# others
+cask "spotify"
+cask "signal"
+cask "slack"
+cask "authy"
+brew "mike-engel/jwt-cli/jwt-cli"
--- bin/aws-ssh-over-ssm.sh
+++ bin/aws-ssh-over-ssm.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env sh
-# based on and all credits to original:
-# https://github.com/qoomon/aws-ssm-ec2-proxy-command/commit/c17b47b66eaf21835f80e26b452b14c525246d4f
-# everything else is my doing but I haven't written original script
-# so all thanks and cheers to @qoomon
-
+######## Source ################################################################
+#
+# https://github.com/qoomon/aws-ssm-ec2-proxy-command
+#
+######## Usage #################################################################
+# https://github.com/qoomon/aws-ssm-ec2-proxy-command/blob/master/README.md
+#
+# Install Proxy Command
+#   - Move this script to ~/.ssh/aws-ssm-ec2-proxy-command.sh
+#   - Ensure it is executable (chmod +x ~/.ssh/aws-ssm-ec2-proxy-command.sh)
+#
+# Add following SSH Config Entry to ~/.ssh/config
+#   host i-* mi-*
+#     IdentityFile ~/.ssh/id_rsa
+#     ProxyCommand ~/.ssh/aws-ssm-ec2-proxy-command.sh %h %r %p ~/.ssh/id_rsa.pub
+#     StrictHostKeyChecking no
+#
+# Ensure SSM Permissions for Target Instance Profile
+#   https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-instance-profile.html
+#
+# Open SSH Connection
+#   ssh <INSTANCE_USER>@<INSTANCE_ID>
+#
+#   Ensure AWS CLI environment variables are set properly
+#   e.g. AWS_PROFILE='default' ssh ec2-user@i-xxxxxxxxxxxxxxxx
+#
+#   If default region does not match instance region you need to provide it like this
+#   ssh <INSTANCE_USER>@<INSTANCE_ID>--<INSTANCE_REGION>
+#
+################################################################################
 set -eu
 
 REGION_SEPARATOR='--'
@@ -12,22 +37,33 @@ ec2_instance_id="$1"
 ssh_user="$2"
 ssh_port="$3"
 ssh_public_key_path="$4"
+ssh_public_key="$(cat "${ssh_public_key_path}")"
+ssh_public_key_timeout=60
 
-if echo "${ec2_instance_id}" | grep -qe "${REGION_SEPARATOR}"; then
+if [[ "${ec2_instance_id}" == *"${REGION_SEPARATOR}"* ]]
+then
   export AWS_DEFAULT_REGION="${ec2_instance_id##*${REGION_SEPARATOR}}"
   ec2_instance_id="${ec2_instance_id%%${REGION_SEPARATOR}*}"
 fi
 
-instance_availability_zone="$(aws ec2 describe-instances \
-  --instance-id "${ec2_instance_id}" \
-  --query "Reservations[0].Instances[0].Placement.AvailabilityZone" \
-  --output text)"
+>/dev/stderr echo "Add public key ${ssh_public_key_path} for ${ssh_user} at instance ${ec2_instance_id} for ${ssh_public_key_timeout} seconds"
+aws ssm send-command \
+  --instance-ids "${ec2_instance_id}" \
+  --document-name 'AWS-RunShellScript' \
+  --comment "Add an SSH public key to authorized_keys for ${ssh_public_key_timeout} seconds" \
+  --parameters commands="\"
+    mkdir -p ~${ssh_user}/.ssh && cd ~${ssh_user}/.ssh || exit 1
 
-aws ec2-instance-connect send-ssh-public-key \
-  --instance-id "${ec2_instance_id}" \
-  --instance-os-user "${ssh_user}" \
-  --ssh-public-key "file://${ssh_public_key_path}" \
-  --availability-zone "${instance_availability_zone}"
+    authorized_key='${ssh_public_key} ssm-session'
+    echo \\\"\${authorized_key}\\\" >> authorized_keys
+
+    sleep ${ssh_public_key_timeout}
+
+    grep -v -F \\\"\${authorized_key}\\\" authorized_keys > .authorized_keys
+    mv .authorized_keys authorized_keys
+  \""
+
+>/dev/stderr echo "Start ssm session to instance ${ec2_instance_id}"
 aws ssm start-session \
   --target "${ec2_instance_id}" \
   --document-name 'AWS-StartSSHSession' \
