72c51b462c35b77a227eb88d72b48b181b46da0a
Tomasz Trebski <kornicameister@gmail.com>
2026-06-23 08:35:48 +0200

    fix(brew): fix rubocop violations in aws session manager formula

--- brew_formula/aws_session_manager.rb
+++ brew_formula/aws_session_manager.rb
@@ -1,24 +1,27 @@
+# frozen_string_literal: true
+
+# Formula for AWS Session Manager Plugin for the AWS CLI.
 class AwsSessionManager < Formula
-  desc "AWS Session Manager Plugin for the AWS CLI"
-  homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  version "latest"
-  license "Apache-2.0"
+  desc 'AWS Session Manager Plugin for the AWS CLI'
+  homepage 'https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html'
+  version 'latest'
+  license 'Apache-2.0'
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/sessionmanager-bundle.zip"
-      sha256 "9c469a186b117747ddb5b9fd8ddccc3fa71f9a55e1f2188b9291d2b237dbe5e7"
+      url 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/sessionmanager-bundle.zip'
+      sha256 '9c469a186b117747ddb5b9fd8ddccc3fa71f9a55e1f2188b9291d2b237dbe5e7'
     else
-      url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip"
-      sha256 "7ed5ecbbe5b84515325e11a886d5b65a6fac41b0d074758de8bad2159302213c"
+      url 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip'
+      sha256 '7ed5ecbbe5b84515325e11a886d5b65a6fac41b0d074758de8bad2159302213c'
     end
   end
 
   def install
-    bin.install "bin/session-manager-plugin"
+    bin.install 'bin/session-manager-plugin'
   end
 
   test do
-    system bin/"session-manager-plugin", "--version"
+    system bin / 'session-manager-plugin', '--version'
   end
 end
